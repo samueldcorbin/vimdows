bfa2c464f72f9cde5a45fbf0a2f069fc40da2268
samueldcorbin <samueldcorbin@gmail.com>
2020-12-11 11:53:36 -0500

    Fix for spaces in file names

--- vim.bat
+++ vim.bat
@@ -1,3 +1,3 @@
 @echo off
-set default-save-location='%USERPROFILE%\Desktop'
-wt wsl -- if [ '%1' = '' ]\; then cd $(wslpath %default-save-location%) ^&^& vim\; else cd $(dirname $(wslpath '%1')) ^&^& vim $(wslpath '%1')\; fi
+set default-save-location=%USERPROFILE%\Desktop
+wt wsl if [ '%~1' = '' ]\; then cd "$(wslpath '%default-save-location%')" ^&^& vim\; else cd "$(wslpath '%~p1')" ^&^& vim "$(wslpath '%~1')" \; fi
