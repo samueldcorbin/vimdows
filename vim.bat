392ff6aff980653c3a7148f3889466f3f53d06b1
samueldcorbin <samueldcorbin@gmail.com>
2020-12-12 06:37:29 -0500

    Better terminal tab title

--- vim.bat
+++ vim.bat
@@ -1,4 +1,9 @@
 @echo off
 set default-save-location=%USERPROFILE%\Desktop
 set wsl-profile-name=Ubuntu
-wt -p "%wsl-profile-name%" wsl bash --login -c 'if [ '\''%~1'\'' = '\'''\'' ]\; then cd "$(wslpath '\''%default-save-location%'\'')" ^&^& vim\; else cd "$(wslpath '\''%~dp1'\'')" ^&^& vim "$(wslpath '\''%~1'\'')" \; fi'
+if "%~1" == "" (
+    wt -p "%wsl-profile-name%" --title "vim" wsl bash --login -c 'cd "$(wslpath '\''%default-save-location%'\'')" ^&^& vim'
+) else (
+    wt -p "%wsl-profile-name%" --title "%~nx1" wsl bash --login -c 'cd "$(wslpath '\''%~dp1'\'')" ^&^& vim "$(wslpath '\''%~1'\'')" '
+)
+pause
